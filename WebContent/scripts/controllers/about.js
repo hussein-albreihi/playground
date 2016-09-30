69710371ceee80b331c490b1ae1c7ce33d92df77
Hussein Al-Breihi <hussein.al.breihi@netinsight.net>
2016-09-30 09:47:39 +0200

    first commit with first website

---
+++ WebContent/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webContentApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webContentApp
+ */
+angular.module('webContentApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ WebContent/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webContentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webContentApp
+ */
+angular.module('webContentApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
